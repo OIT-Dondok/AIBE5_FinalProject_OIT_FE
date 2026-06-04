69adcc98e7773fcc33fcc39399accf56972e3f74
seoilhyeon <ilhyeon0326@gmail.com>
2026-06-04 23:23:22 +0900

    fix: 도딘 내역 페이지를 server component로 변경 (#59)

--- src/app/my/dodin/history/page.tsx
+++ src/app/my/dodin/history/page.tsx
@@ -1,17 +1,10 @@
-"use client";
-
-import { useMemo } from "react";
-
 import { Header } from "@/components/common/Header";
 import { DodinHistoryList } from "@/components/domain/point/DodinHistoryList";
 import { createWalletViewModel } from "@/components/domain/point/pointViewModel";
 import { mockPointAccount, mockPointHistory } from "@/mocks/data/points";
 
 export default function DodinHistoryPage() {
-  const walletHistory = useMemo(
-    () => createWalletViewModel(mockPointAccount, mockPointHistory.items),
-    [],
-  );
+  const walletHistory = createWalletViewModel(mockPointAccount, mockPointHistory.items);
 
   return (
     <main className="min-h-screen w-full overflow-x-hidden bg-transparent">
