d975dcf9929c0f20167f1a9fb35a5de34d2e3e26
jeonseong0120 <jeonseong922@gmail.com>
2026-06-20 22:31:27 +0900

    fix : coderabbit 피드백 반영

---
+++ src/components/common/ReceiptTopEdge.tsx
@@ -0,0 +1,16 @@
+export function ReceiptTopEdge() {
+  return (
+    <svg
+      aria-hidden="true"
+      className="w-full"
+      height="12"
+      viewBox="0 0 200 12"
+      preserveAspectRatio="none"
+    >
+      <path
+        d="M0,12 L0,8 L5,0 L10,8 L15,0 L20,8 L25,0 L30,8 L35,0 L40,8 L45,0 L50,8 L55,0 L60,8 L65,0 L70,8 L75,0 L80,8 L85,0 L90,8 L95,0 L100,8 L105,0 L110,8 L115,0 L120,8 L125,0 L130,8 L135,0 L140,8 L145,0 L150,8 L155,0 L160,8 L165,0 L170,8 L175,0 L180,8 L185,0 L190,8 L195,0 L200,8 L200,12 Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
